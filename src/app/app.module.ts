aff51adc6330ecd418a4aaee7528d2fb6772d49c
Thiago Moura <thiagogcm@gmail.com>
2017-09-03 16:57:13 -0300

    Adding components skeletons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { InnerPortfolioComponent } from './inner-portfolio/inner-portfolio.component';
+import { InnerServicesComponent } from './inner-services/inner-services.component';
+import { InnerBlogComponent } from './inner-blog/inner-blog.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    InnerPortfolioComponent,
+    InnerServicesComponent,
+    InnerBlogComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
